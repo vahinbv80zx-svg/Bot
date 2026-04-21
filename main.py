6bc909914cda5cea2880a26045eac5d40ee05431
vahinbv80zx-svg <vahinbv.80zx@gmail.com>
2026-04-21 19:46:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ async def on_message(message):
     if message.content == "!ping":
         await message.channel.send("Pong!")
 
-client.run(os.getenv("MTQ5NjE0OTM5NTcyNzU4NTM4MQ.G3Q0J7.DvIiAFWv-36NykQvJI3IKXlgA1pt3yKXCZkZAo"))
+client.run(os.getenv("DISCORD_TOKEN"))
